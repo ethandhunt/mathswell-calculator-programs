50f459f4fcb132d9489d6b9ac08a8355c9a50776
Ethan <ethandhunt@gmail.com>
2023-08-19 19:31:12 +1200

    print multiples

--- mul_in_range.py
+++ mul_in_range.py
@@ -5,4 +5,7 @@ while 1:
     low = int(input('lo: '))
     high = int(input('hi: '))
 
+    for x in range(math.ceil(low/k), math.ceil(high/k)):
+        print(k*x)
+        
     input('=' + str(math.ceil(high/k) - math.ceil(low/k)))
