9df3be64273e4de82675e28c16893abd842ce68f
Ethan <ethandhunt@gmail.com>
2023-08-19 15:56:57 +1200

    sudo runs it faster

--- send.sh
+++ send.sh
@@ -4,4 +4,4 @@ rm /run/media/ethan/disk/mathswell/*.py
 echo 'saving new files...'
 cp *.py /run/media/ethan/disk/mathswell/
 echo 'ejecting...'
-eject /run/media/ethan/disk/
+sudo eject /run/media/ethan/disk/
